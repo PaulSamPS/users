e7c8fa59f9090d699f3011e5668b247b7ade2799
Pavel Samoylenko <paulsamv@yandex.ru>
2024-06-30 00:48:34 +0300

    tokens

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
+import { TokensModule } from './tokens/tokens.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [UsersModule, TokensModule],
 })
 export class AppModule {}
